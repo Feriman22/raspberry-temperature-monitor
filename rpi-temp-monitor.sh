99047357761a1f5f05da66e5d0bd761b5ceed1b0
Feriman <Feriman22@users.noreply.github.com>
2020-04-25 22:02:19 +0200

    Add files via upload

--- rpi-temp-monitor.sh
+++ rpi-temp-monitor.sh
@@ -2,49 +2,19 @@
 SCRIPTNAME="Raspberry Temperature Monitor"
 VERSION="25-04-2020"
 SCRIPTLOCATION="/usr/local/sbin/rpi-temp-monitor.sh"
-PROFILELOCATION="/etc/profile.d/rpi-temp-monitor"
+PROFILELOCATION="/etc/profile.d/rpi-temp-monitor-alias.sh"
 
 #
-# Define some functions
+# Define function
 #
-
-CHECK()
-{
-PS3='Check temperature of RPi4. Please enter your choice: '
-OPT1="Live Watch - Refresh every 0.2 sec"
-OPT2="Write to terminal - Refresh every 2 sec"
-OPT3="Quit"
-
-options=("$OPT1" "$OPT2" "$OPT3")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "$(echo $OPT1)")
-            watch -n 0.2 /opt/vc/bin/vcgencmd measure_temp
-            ;;
-        "$(echo $OPT2)")
-		while [[ 1 = 1 ]]; do
-		/opt/vc/bin/vcgencmd measure_temp | cut -c 6-
-		date
-		echo
-		sleep 2
-		done
-            ;;
-        "$(echo $OPT3)")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
-}
 
 UPDATE()
 {
 	# Getting info about the latest GitHub version
 	NEW=$(curl -L --silent "https://github.com/Feriman22/raspberry-temperature-monitor/releases/latest" | awk '/<title>Release/ {print $4}')
-	
+
 	# Compare the installed and the GitHub stored version - Only internal, not available by any argument
-	if [[ "$1" == "ONLYCHECK" ]] && [[ "$NEW" != "$VERSION" ]]; then
+	if [[ "$1" == "ONLYCHECK" ]] && [[ "$NEW" != "$VERSION" ]] && [[ "$NEW" =~ '-' ]]; then
 		echo -e "New version ${GR}available!${NC}"
 	else
 		[[ "$1" == "ONLYCHECK" ]] && echo -e "The script is ${GR}up to date.${NC}"
@@ -57,7 +27,7 @@ UPDATE()
 		[[ ! "$NEW" ]] && echo -e "GitHub is ${RED}not available now.${NC} Try again later." && exit
 
 		# Compare the installed and the GitHub stored version
-		if [[ "$NEW" != "$(awk '/VERSION=/' "$SCRIPTLOCATION" | grep -o -P '(?<=").*(?=")')" ]]; then
+		if [[ "$NEW" != "$(awk '/VERSION=/' "$SCRIPTLOCATION" | grep -o -P '(?<=").*(?=")')" ]] && [[ "$NEW" =~ '-' ]]; then
 			wget -q https://raw.githubusercontent.com/Feriman22/raspberry-temperature-monitor/rpi-temp-monitor.sh -O $SCRIPTLOCATION
 			echo -e "Script has been ${GR}updated.${NC}"
 		else
@@ -68,58 +38,106 @@ UPDATE()
 	fi
 }
 
-if [ "$1" != '--cron' ]; then
-	# Coloring
-	RED='\033[0;31m' # Red Color
-	GR='\033[0;32m' # Green Color
-	YL='\033[0;33m' # Yellow Color
-	NC='\033[0m' # No Color
 
-	echo -e "\n$SCRIPTNAME\n"
-	echo "Author: Feriman"
-	echo "URL: https://github.com/Feriman22/raspberry-temperature-monitor"
-	echo "Open GitHub page to read the manual and check new releases"
-	echo "Version: $VERSION"
-	UPDATE ONLYCHECK # Check new version
-	echo -e "${GR}If you found it useful, please donate via PayPal: https://paypal.me/BajzaFerenc${NC}\n"
-fi
+# Coloring
+RED='\033[0;31m' # Red Color
+GR='\033[0;32m' # Green Color
+YL='\033[0;33m' # Yellow Color
+NC='\033[0m' # No Color
+
+# Print welcome text
+echo -e "\n$SCRIPTNAME\n"
+echo "Author: Feriman"
+echo "URL: https://github.com/Feriman22/raspberry-temperature-monitor"
+echo "Open GitHub page to read the manual and check new releases"
+echo "Version: $VERSION"
+UPDATE ONLYCHECK # Check new version
+echo -e "${GR}If you found it useful, please donate via PayPal: https://paypal.me/BajzaFerenc${NC}\n"
 
 
 # Call the menu
-if [ "$1" == "-i" ] || [ "$1" == "-u" ] || [ "$1" == "-v" ] || [ "$1" == "--install" ] || [ "$1" == "--uninstall" ] || [ "$1" == "--verify" ] || [ "$1" == "-up" ] || [ "$1" == "--update" ]; then
+if [ "$1" == "-i" ] || [ "$1" == "-u" ] || [ "$1" == "-p" ] || [ "$1" == "--install" ] || [ "$1" == "--uninstall" ] || [ "$1" == "--portable" ] || [ "$1" == "-up" ] || [ "$1" == "--update" ]; then
 	OPT="$1" && OPTL="$1" && ARG="YES"
 else
-	PS3='Please enter your choice: '
-	options=("Portable (any user)" "Install (only root)" "Uninstall (only root)" "Update (only root)" "Quit")
-	select opt in "${options[@]}"
-	do
-		case $opt in
-			"Portable")
-				OPT='-p' && OPTL='--portable' && break
-				;;
-			"Install")
-				OPT='-i' && OPTL='--install' && break
-				;;
-			"Uninstall")
-				OPT='-u' && OPTL='--uninstall' && break
-				;;
-			"Update")
-				OPT='-up' && OPTL='--update' && break
-				;;
-			"Quit")
-				break
-				;;
-			*) echo "Invalid option $REPLY";;
-		esac
-	done
+	# Check the script is installed or not. Print the correct menu
+	if [ -f "$PROFILELOCATION" ] && [ -x "$SCRIPTLOCATION" ]; then
+		# The script is installed
+		PS3='Please enter your choice: '
+		options=("Run the monitor (any user)" "Uninstall (only root)" "Update (only root)" "Quit")
+		select opt in "${options[@]}"
+		do
+			case $opt in
+				"Run the monitor (any user)")
+					OPT='-p' && OPTL='--portable' && break
+					;;
+				"Uninstall (only root)")
+					OPT='-u' && OPTL='--uninstall' && break
+					;;
+				"Update (only root)")
+					OPT='-up' && OPTL='--update' && break
+					;;
+				"Quit")
+					break
+					;;
+				*) echo "Invalid option $REPLY";;
+			esac
+		done
+	else
+		# The script is not installed.
+		PS3='Please enter your choice: '
+		options=("Portable (any user)" "Install (only root)" "Quit")
+		select opt in "${options[@]}"
+		do
+			case $opt in
+				"Portable (any user)")
+					OPT='-p' && OPTL='--portable' && break
+					;;
+				"Install (only root)")
+					OPT='-i' && OPTL='--install' && break
+					;;
+				"Quit")
+					break
+					;;
+				*) echo "Invalid option $REPLY";;
+			esac
+		done
+	fi
 fi
 
 
 ##
-########### Choosed the Portable ###########
+########### Choosed the Portable or Run the monitor ###########
 ##
 
-[ "$OPT" == '-p' ] || [ "$OPTL" == '--portable' ] && CHECK
+if [ "$OPT" == '-p' ] || [ "$OPTL" == '--portable' ]; then
+	PS3='There is three options. Please enter your choice: '
+	OPT1="Live Watch - Refresh every 0.2 sec"
+	OPT2="Write to terminal - Refresh every 2 sec"
+	OPT3="Quit"
+
+	options=("$OPT1" "$OPT2" "$OPT3")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"$(echo $OPT1)")
+				watch -n 0.2 /opt/vc/bin/vcgencmd measure_temp
+				;;
+			"$(echo $OPT2)")
+				echo -e "Press Ctrl + C to exit.\n"
+				while [[ 1 = 1 ]]; do
+				/opt/vc/bin/vcgencmd measure_temp | cut -c 6-
+				date
+				echo
+				sleep 2
+				done
+				;;
+			"$(echo $OPT3)")
+				break
+				;;
+			*) echo "invalid option $REPLY";;
+		esac
+	done
+fi
 
 
 ##
@@ -138,8 +156,25 @@ if [ "$OPT" == '-i' ] || [ "$OPTL" == '--install' ]; then
 	# Start count the time of install process
 	SECONDS=0
 
-	# Set alias in profile if doesn't exists yet
-	[ ! -f "$PROFILELOCATION" ] && echo -e "alias temp="$SCRIPTLOCATION"" > "$PROFILELOCATION" && echo -e "Alias has been set. ${GR}OK.${NC}" || echo -e "Alias ${GR}already set.${NC}"
+	# Set alias in profile for all users
+	STRING="alias temp="$SCRIPTLOCATION"" # Used for root as well
+	if [ ! -f "$PROFILELOCATION" ]; then
+		echo -e "$STRING\nalias sudo='sudo '" > "$PROFILELOCATION"
+		chmod +x "$PROFILELOCATION"
+		echo -e "Alias has been set for all users. ${GR}OK.${NC}"
+	else
+		echo -e "Alias ${GR}already set for all users.${NC}"
+	fi
+	
+	# Set alias in bashrc for root
+	STRING="alias temp="$SCRIPTLOCATION""
+	if [ $(grep -c "$STRING" /root/.bashrc) -lt "1" ]; then
+		echo "$STRING" >> /root/.bashrc
+		. /root/.bashrc
+		echo -e "Alias has been set for root. ${GR}OK.${NC}"
+	else
+		echo -e "Alias ${GR}already set for root.${NC}"
+	fi
 
 	# Copy the script to $SCRIPTLOCATION and add execute permission
 	if [ "$(dirname "$0")/$(basename "$0")" != "$SCRIPTLOCATION" ]; then
@@ -149,10 +184,11 @@ if [ "$OPT" == '-i' ] || [ "$OPTL" == '--install' ]; then
 	fi
 
 	# Activate the alias
-	. $PROFILELOCATION
+	source /etc/profile
 
 	# Happy ending.
 	echo -e "${GR}Done.${NC} Full install time was $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+
 fi
 
 
@@ -191,11 +227,23 @@ if [ "$OPT" == '-u' ] || [ "$OPTL" == '--uninstall' ]; then
 	# Remove profile file
 	if [ -f "$PROFILELOCATION" ]; then
 		rm -r "$PROFILELOCATION"
-		echo -e "\nAlias removed. ${GR}OK.${NC}"
+		echo -e "\nAlias removed for all users. ${GR}OK.${NC}"
 	else
-		echo -e "\nAlias not found. ${GR}OK.${NC}"
+		echo -e "\nAlias not found for all users. ${GR}OK.${NC}"
+	fi
+	
+	# Remove alias in .bashrc
+	if [ $(grep -c "$SCRIPTLOCATION" /root/.bashrc) -gt "0" ]; then
+		sed -i '/alias temp=/d' /root/.bashrc
+		. /root/.bashrc
+		echo -e "\nAlias removed for root. ${GR}OK.${NC}"
+	else
+		echo -e "\nAlias not found for root. ${GR}OK.${NC}"
 	fi
 
+	# Disable the alias
+	source /etc/profile
+	
 	# Remove the script
 	[ -f "$SCRIPTLOCATION" ] && rm -f "$SCRIPTLOCATION" && echo -e "The script removed. ${GR}OK.${NC}" || echo -e "Script not found. ${GR}OK.${NC}"
 
@@ -206,4 +254,10 @@ fi
 ########### Choosed the Update ###########
 ##
 
-[ "$OPT" == '-up' ] || [ "$OPTL" == '--update' ] && [ ! $(id -u) = 0 ] && echo -e "${RED}Run as root!${NC} Exiting...\n" && exit || UPDATE
+if [ "$OPT" == '-up' ] || [ "$OPTL" == '--update' ]; then
+
+	# Check the root permission
+	[ ! $(id -u) = 0 ] && echo -e "${RED}Run as root!${NC} Exiting...\n" && exit
+	UPDATE
+
+fi
